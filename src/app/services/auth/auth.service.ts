011d977e61da68d7fb8c5938b64ff543e5061a9b
nut <nut999anan@hotmail.com>
2024-12-02 15:16:51 +0700

    login 50%

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,11 +1,20 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
+  private readonly TOKEN_KEY = 'authToken';
   isLoggedIn = false;
   url = 'http://localhost:3000/login';
+
+  constructor(@Inject(PLATFORM_ID) private platformId: object) {
+    if (isPlatformBrowser(this.platformId)) {
+      const token = localStorage.getItem(this.TOKEN_KEY);
+      this.isLoggedIn = !!token;
+    }
+  }
 
   isAuthenticated(): boolean {
     return this.isLoggedIn;
@@ -14,6 +23,9 @@ export class AuthService {
   async login(username: string, password: string): Promise<boolean> {
     if (await this.isValidCredentials(username, password)) {
       this.isLoggedIn = true;
+      if (isPlatformBrowser(this.platformId)) {
+        localStorage.setItem(this.TOKEN_KEY, 'true');
+      }
       return true;
     }
     return false;
@@ -21,6 +33,9 @@ export class AuthService {
 
   logout() {
     this.isLoggedIn = false;
+    if (isPlatformBrowser(this.platformId)) {
+      localStorage.removeItem(this.TOKEN_KEY);
+    }
   }
 
   async isValidCredentials(
@@ -34,8 +49,8 @@ export class AuthService {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-          username: username,
-          password: password,
+          username,
+          password,
         }),
       });
 
